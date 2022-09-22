97be797255e78e251e5b0e89a63d7594eaf52cc6
Sharan Chitta <chitta.sharan@gmail.com>
2022-09-21 21:32:30 -0400

    Add Users app and Sensors Models

--- infra/models.py
+++ infra/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Session(models.Model):
+    pass
+
+
+class IRSensorData(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    session = models.ForeignKey(
+        Session,
+        on_delete=models.CASCADE,
+        verbose_name="Session"
+    )
+
+
+class InertialSensorData(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    session = models.ForeignKey(
+        Session,
+        on_delete=models.CASCADE,
+        verbose_name="Session"
+    )
+    l5s1_lateral = models.FloatField(null=True, blank=True)
+    l5s1_axial = models.FloatField(null=True, blank=True)
+    l5s1_flexion = models.FloatField(null=True, blank=True)
+
+    l4l3_lateral = models.FloatField(null=True, blank=True)
+    l4l3_axial = models.FloatField(null=True, blank=True)
+    l4l3_flexion = models.FloatField(null=True, blank=True)
+
+    l1t12_lateral = models.FloatField(null=True, blank=True)
+    l1t12_axial = models.FloatField(null=True, blank=True)
+    l1t12_flexion = models.FloatField(null=True, blank=True)
+
+    t9t8_lateral = models.FloatField(null=True, blank=True)
+    t9t8_axial = models.FloatField(null=True, blank=True)
+    t9t8_flexion = models.FloatField(null=True, blank=True)
+
+    t1c7_lateral = models.FloatField(null=True, blank=True)
+    t1c7_axial = models.FloatField(null=True, blank=True)
+    t1c7_flexion = models.FloatField(null=True, blank=True)
+
+    c1head_lateral = models.FloatField(null=True, blank=True)
+    c1head_axial = models.FloatField(null=True, blank=True)
+    c1head_flexion = models.FloatField(null=True, blank=True)
+
+
+class SEMGSensorData(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    session = models.ForeignKey(
+        Session,
+        on_delete=models.CASCADE,
+        verbose_name="Session"
+    )
+    rightc4_paraspinal = models.FloatField(null=True, blank=True)
+    leftc4_paraspinal = models.FloatField(null=True, blank=True)
+    right_multifidus = models.FloatField(null=True, blank=True)
+    left_multifidus = models.FloatField(null=True, blank=True)
